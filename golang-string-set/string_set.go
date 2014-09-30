fbff6db029dbaba79d98b2a71359e3568bcb3e73
Dorsey Dick <djdick@andrew.cmu.edu>
2014-09-30 11:39:29 -0400

    Starting Fresh

---
+++ golang-string-set/string_set.go
@@ -0,0 +1,49 @@
+package string_set
+
+import "fmt"
+
+type StringSet map[string]bool
+
+func NewStringSet() StringSet {
+	return make(StringSet)
+}
+func (s StringSet) Add(val string) {
+	s[val] = true
+}
+func (s StringSet) AddAll(src StringSet) {
+	for k, _ := range src {
+		s[k] = true
+	}
+}
+func (s StringSet) String() string {
+	return fmt.Sprint(s.AsSlice()) // could be made more efficient if needed
+}
+
+func (s StringSet) AsSlice() []string {
+	ret := make([]string, 0, len(s))
+	for k, _ := range s {
+		ret = append(ret, k)
+	}
+	return ret
+}
+
+func (s StringSet) Contains(i ...string) bool {
+	for _, val := range i {
+		if _, ok := s[val]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func (s StringSet) Difference(other StringSet) StringSet {
+	//_ = other.(*threadUnsafeSet)
+
+	difference := NewStringSet()
+	for elem := range s {
+		if !other.Contains(elem) {
+			difference.Add(elem)
+		}
+	}
+	return difference
+}
